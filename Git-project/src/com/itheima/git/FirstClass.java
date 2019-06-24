1d12ef37c45b4b778e939869931911057273d353
mty <172173600@qq.com>
2019-06-24 09:26:19 +0800

    添加了修改内容

--- Git-project/src/com/itheima/git/FirstClass.java
+++ Git-project/src/com/itheima/git/FirstClass.java
@@ -7,4 +7,10 @@ public class FirstClass {
         System.out.println("第一个Git project");
         System.out.println("第一个Git project");
     }
+
+    public void test()
+    {
+        int i = 0;
+        i += 10;
+    }
 }
