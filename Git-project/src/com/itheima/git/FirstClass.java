35e5d070bdd0fdff4fabb785170cf0dbce54e109
mty <172173600@qq.com>
2019-06-24 09:23:48 +0800

    把本地工程添加到Git

---
+++ Git-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("第一个Git project");
+        System.out.println("第一个Git project");
+        System.out.println("第一个Git project");
+        System.out.println("第一个Git project");
+    }
+}
